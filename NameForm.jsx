f525ad5d68f70739949193b0dccd783664aa4cc1
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-06 13:12:10 +0330

    Update NameForm.jsx

--- NameForm.jsx
+++ NameForm.jsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 
 export default function NameForm() {
   const [name, setName] = useState("");
+  const [age , setAge] = useState("");
+  const [submittedAge, setSubmittedAge] = useState("");
   const [submittedName, setSubmittedName] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault(); // نذار فرم صفحه رو رفرش کنه
     setSubmittedName(name);
+    setSubmittedAge(age);
   };
 
   return (
@@ -18,10 +21,16 @@ export default function NameForm() {
           onChange={(e) => setName(e.target.value)}
           placeholder="نام خود را وارد کنید"
         />
+        <input
+            type = "text"
+            value = {age}
+            onChange = {(e) => setAge(e.target.value)}
+            placeholder = "Enter your Age: "
+        />
         <button type="submit">ثبت</button>
       </form>
 
-      {submittedName && <h2>سلام {submittedName}!</h2>}
+      {<h2>سلام {submittedName} سن تو {submittedAge} سال است.</h2>}
     </div>
   );
 }
