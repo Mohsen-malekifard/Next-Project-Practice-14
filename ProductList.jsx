dd1229ad7769281cd12f8ce7049925fd06567eb9
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-06 13:49:35 +0330

    Update ProductList.jsx

--- ProductList.jsx
+++ ProductList.jsx
@@ -1,20 +1,28 @@
-const products = [
-  { id: 1, name: "لپ‌تاپ", price: 100 },
-  { id: 2, name: "گوشی", price: 200 },
-  { id: 3, name: "تبلت", price: 300 },
-];
+import { useState } from "react";
 
 export default function ProductList() {
+  const [products, setProducts] = useState([
+    { id: 1, name: "لپ‌تاپ", price: 100 },
+    { id: 2, name: "گوشی", price: 200 },
+    { id: 3, name: "تبلت", price: 300 },
+  ]);
+
   return (
-    <ul>
-      {products.map((p) => (
-        <li key={p.id}>
-          {p.name} —{" "}
-          <span style={{ color: p.price > 150 ? "red" : "black" }}>
-            {p.price} دلار
-          </span>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <button onClick={() => setProducts(products.filter((p) => p.price > 150))}>
+        فقط قیمت‌های بالای ۱۵۰
+      </button>
+
+      <ul>
+        {products.map((p) => (
+          <li key={p.id}>
+            {p.name} —{" "}
+            <span style={{ color: p.price > 150 ? "red" : "black" }}>
+              {p.price} دلار
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
