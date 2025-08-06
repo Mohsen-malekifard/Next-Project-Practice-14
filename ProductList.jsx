8a7c80f0554131564b429ffb76947f4a6329d4cd
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-06 13:35:54 +0330

    Update ProductList.jsx

--- ProductList.jsx
+++ ProductList.jsx
@@ -1,14 +1,19 @@
 const products = [
-  { id: 1, name: "لپ‌تاپ", price:100 },
-  { id: 2, name: "گوشی" , price :200},
-  { id: 3, name: "تبلت", price : 300 },
+  { id: 1, name: "لپ‌تاپ", price: 100 },
+  { id: 2, name: "گوشی", price: 200 },
+  { id: 3, name: "تبلت", price: 300 },
 ];
 
 export default function ProductList() {
   return (
     <ul>
       {products.map((p) => (
-        <li key = {p.id} >{p.name}-{p.price}</li>
+        <li key={p.id}>
+          {p.name} —{" "}
+          <span style={{ color: p.price > 150 ? "red" : "black" }}>
+            {p.price} دلار
+          </span>
+        </li>
       ))}
     </ul>
   );
